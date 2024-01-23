75cbe048f2c5f631c39f41fbe53e558787232212
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-23 22:30:12 +0300

    Printed cities using another api

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,23 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://data.cityofchicago.org/resource/f7f2-ggz5.json"
 
     response = requests.get(URL)
     return response.content
+  
+  def program_cities(self):
+    programs_list= []
+    programs= json.loads(self.get_programs())
+    for program in programs:
+      programs_list.append(program['city'])
+    return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+
+# programs = GetPrograms().get_programs()
+# print(programs)
+programs= GetPrograms()
+cities= programs.program_cities()
+
+for city in set(cities):
+  print(city)
